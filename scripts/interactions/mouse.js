c13c1f5965b494f10d426d39871a60b0f7de4f2c
A. Akbar Hidayat <keripix@gmail.com>
2013-05-23 16:36:15 +0700

    preventDefault event handler untuk event selectstart pada canvas

--- scripts/interactions/mouse.js
+++ scripts/interactions/mouse.js
@@ -13,6 +13,10 @@ define(function(){
 
   Mouse.prototype = {
     init: function(){
+      this.canvas.addEventListener('selectstart', function(e){
+        e.preventDefault();
+        return false;
+      });
       this.canvas.addEventListener('mousedown', this.onMouseDown.bind(this), false);
       this.canvas.addEventListener('mouseup', this.onMouseUp.bind(this), false);
       this.canvas.addEventListener('mousemove', this.onMouseMove.bind(this), false);
