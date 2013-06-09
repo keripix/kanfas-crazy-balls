ed57543eb598f7d8649db9cdb9159b7f0d53cfcb
A. Akbar Hidayat <keripix@gmail.com>
2013-06-09 12:15:28 +0700

    simplify shapefactory

--- libs/shapes/shapefactory.js
+++ libs/shapes/shapefactory.js
@@ -4,19 +4,17 @@ define([
   ],
 function(Rectangle, Circle){
 
-  function create(type, config, events) {
-    type = type.toLowerCase();
-
-    if (type === "rectangle") {
-      return Rectangle.create(config);
-    } else if (type === "circle") {
-      return Circle.create(config);
-    } else if (type === "image") {
-      console.log("image");
-    }
-  }
-
   return {
-    create: create
+    create: function (type, config) {
+      type = type.toLowerCase();
+
+      if (type === "rectangle") {
+        return Rectangle.create(config);
+      } else if (type === "circle") {
+        return Circle.create(config);
+      } else if (type === "image") {
+        console.log("image");
+      }
+    }
   };
 });
