f10399c5c48ea9274676660b5166c77b55241e0f
A. Akbar Hidayat <keripix@gmail.com>
2013-06-11 01:03:47 +0700

    begin test

--- tests/specs/kanfas.specs.js
+++ tests/specs/kanfas.specs.js
@@ -1,11 +1,24 @@
-define(["kanfas"],
-function(Kanfas){
+define(["kanfas", "state","shapes/shapefactory"],
+function(Kanfas, State,ShapeFactory){
   var canvas = document.getElementById('testcanvas'),
-      kanfas = Kanfas.create(canvas);
+      state = State.init(canvas, {}),
+      kanfas = Kanfas.create(canvas, state);
 
   describe("Kanfas", function(){
-    it("Should", function(){
-      expect(kanfas instanceof Kanfas).toBe(true);
+    describe("Adding objects to kanfas", function(){
+      it("Should have the corect number of items", function(){
+        kanfas.add(ShapeFactory.create("Rectangle",{
+          x: 0,y:0
+        }));
+        kanfas.add(ShapeFactory.create("Rectangle",{
+          x: 0,y:0
+        }));
+        kanfas.add(ShapeFactory.create("Rectangle",{
+          x: 0,y:0
+        }));
+
+        expect(kanfas.state.objects.length).toEqual(3);
+      });
     });
   });
 });
