816011436d86cdc5feab05aac56839e5e0f0a111
A. Akbar Hidayat <keripix@gmail.com>
2013-06-11 14:33:52 +0700

    use ObjectUtil.addMethods() to State

--- libs/state.js
+++ libs/state.js
@@ -9,66 +9,69 @@
  * 3. Wether a dragging action is being performed
  * 4. What to draw when the canvas request for a redraw
  */
-define(function(){
+define(["utils/objectutil"],
+function(ObjectUtil){
   function State(){
     this.objects = []; // Objects on the canvas
     this.dragging = false; // Am I in dragging mode at the moment?
     this.selections = []; // The selected objects
   }
 
-  State.prototype.getObjects = function(){
-    return this.objects;
-  };
+  ObjectUtil.addMethods(State, {
+    getObjects: function(){
+      return this.objects;
+    },
 
-  State.prototype.getSelected = function(){
-    return this.selections;
-  };
+    getSelected: function(){
+      return this.selections;
+    },
 
-  State.prototype.addObject = function(obj){
-    this.objects.push(obj);
-    return this;
-  };
+    addObject: function(obj){
+      this.objects.push(obj);
+      return this;
+    },
 
-  State.prototype.addSelected = function(obj){
-    if (this.findObjectPos(obj, this.selections) === -1) {
-      this.selections.push(obj);
-    }
-    return this;
-  };
-
-  State.prototype.removeObject = function(obj){
-    var index = this.findObjectPos(obj, this.objects);
-
-    if (index === -1){
-      return;
-    }
-
-    return this.objects.splice(index, 1)[0];
-  };
-
-  State.prototype.removeSelected = function(obj){
-    var index = this.findObjectPos(obj, this.selections);
-
-    if (index === -1) {
-      return;
-    }
-
-    return this.selections.splice(index, 1)[0];
-  };
-
-  State.prototype.findObjectPos = function(obj, data){
-    var length = data.length,
-        index = -1;
-
-    for(var i = length;i >= 0; i--) {
-      if (data[i] === obj) {
-        index = i;
-        break;
+    addSelected: function(obj){
+      if (this.findObjectPos(obj, this.selections) === -1) {
+        this.selections.push(obj);
       }
-    }
+      return this;
+    },
 
-    return index;
-  };
+    removeObject: function(obj){
+      var index = this.findObjectPos(obj, this.objects);
+
+      if (index === -1){
+        return;
+      }
+
+      return this.objects.splice(index, 1)[0];
+    },
+
+    removeSelected: function(obj){
+      var index = this.findObjectPos(obj, this.selections);
+
+      if (index === -1) {
+        return;
+      }
+
+      return this.selections.splice(index, 1)[0];
+    },
+
+    findObjectPos: function(obj, data){
+      var length = data.length,
+          index = -1;
+
+      for(var i = length;i >= 0; i--) {
+        if (data[i] === obj) {
+          index = i;
+          break;
+        }
+      }
+
+      return index;
+    }
+  });
 
   return {
     init: function(){
