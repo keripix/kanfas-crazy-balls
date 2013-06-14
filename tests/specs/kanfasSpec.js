0ea7ddba7f94d2e2d821ff4c6d59d7d3ed0f38c6
A. Akbar Hidayat <keripix@gmail.com>
2013-06-14 22:29:46 +0700

    state

--- tests/specs/kanfasSpec.js
+++ tests/specs/kanfasSpec.js
@@ -7,22 +7,27 @@ function(Kanfas, State,ShapeFactory){
   };
 
   var state = new State(canvas, {}),
-      kanfas = new Kanfas(canvas, state);
+      kanfas = new Kanfas(canvas, state),
+      s1 = ShapeFactory.create("Rectangle", {x:0,y:0}),
+      s2 = ShapeFactory.create("Rectangle", {x:10,y:10}),
+      s3 = ShapeFactory.create("Rectangle", {x:50,y:50});
 
   describe("Kanfas", function(){
-    describe("Adding objects to kanfas", function(){
-      it("Should have the correct number of items", function(){
-        kanfas.add(ShapeFactory.create("Rectangle",{
-          x: 0,y:0
-        }));
-        kanfas.add(ShapeFactory.create("Rectangle",{
-          x: 0,y:0
-        }));
-        kanfas.add(ShapeFactory.create("Rectangle",{
-          x: 0,y:0
-        }));
+    describe("Object collections", function(){
+      it("Should be able to add properly", function(){
+        kanfas.add(s1, s2, s3);
 
         expect(kanfas.state.objects.length).toEqual(3);
+      });
+
+      it("Should be able to remove properly", function(){
+        kanfas.remove(s2);
+
+        expect(kanfas.state.objects.length).toEqual(2);
+
+        kanfas.remove(s3);
+        kanfas.remove(s1);
+        expect(kanfas.state.objects.length).toEqual(0);
       });
     });
   });
