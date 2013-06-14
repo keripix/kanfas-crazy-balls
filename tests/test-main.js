e6b0b811745dca0de5b2c486446ffca2449a0c2c
A. Akbar Hidayat <keripix@gmail.com>
2013-06-14 20:10:48 +0700

    menggunakan karma

---
+++ tests/test-main.js
@@ -0,0 +1,13 @@
+var tests = Object.keys(window.__karma__.files).filter(function (file) {
+  return /Spec\.js$/.test(file);
+});
+
+requirejs.config({
+  baseUrl: "/base/libs",
+
+  // ask Require.js to load these files (all our tests)
+  deps: tests,
+
+  // start test run, once Require.js is done
+  callback: window.__karma__.start
+});
